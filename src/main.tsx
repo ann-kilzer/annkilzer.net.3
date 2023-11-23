71ba61cd37bc9971796ec2e35d76191c4ca67665
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-23 23:59:14 +0900

    react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom/client'
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from '@emotion/react'
 import theme from './theme/theme'
-import ThemePreview from './routes/ThemePreview/ThemePreview'
+import RouterLayout from './routes/RouterLayout';
 import './index.css'
 
 import '@fontsource/literata/500.css';
 import '@fontsource/noto-sans-jp/400.css'
 import { GlobalStyles } from '@mui/material';
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <GlobalStyles styles={{ body: { backgroundColor: '#DAC9A6' } }} />
-            <ThemePreview />
+            <RouterLayout />
         </ThemeProvider>
     </React.StrictMode>,
 )
---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import Container from "@mui/material/Container"
+
+const Home: FC = () => {
+    return <Container>Todo</Container>
+}
+
+export default Home
