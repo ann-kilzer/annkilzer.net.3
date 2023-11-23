b3843fa5431a81192579cfa64cdcc873d495fe9e
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-23 18:54:34 +0900

    MUI, icon, and font install

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '@fontsource/literata/500.css';
+import '@fontsource/noto-sans-jp/400.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
