d05c5490c732e05539a52a9b393a519c442e82da
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-26 00:27:58 +0900

    set up a backup

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // todo: remove me later
-console.log(process.env.PLAYWRIGHT_BASE_URL)
+console.log('base url:', process.env.PLAYWRIGHT_BASE_URL)
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.PLAYWRIGHT_BASE_URL ?? 'http://localhost:5173/',
+        baseURL: process.env.PLAYWRIGHT_BASE_URL ?? process.env.CI ? 'https://annkilzer.com' : 'http://localhost:5173/',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
