83afa67b8133ce36d9d561b4c579a05c948d8a41
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-25 14:32:48 +0900

    ignore html in linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react/jsx-runtime'
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    ignorePatterns: ['dist', '.eslintrc.cjs', '*.html'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: ["tsconfig.json", "tsconfig.node.json"]
