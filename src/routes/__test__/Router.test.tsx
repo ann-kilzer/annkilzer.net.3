d3cdbd53f49f321897476da03e7f4ccc18ce4a1f
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-25 18:34:43 +0900

    remove build error

--- src/routes/__test__/Router.test.tsx
+++ src/routes/__test__/Router.test.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { describe, expect, it } from 'vitest'
-import { render } from '../../tests/customRender'
-import { screen } from '@testing-library/react'
-import Router from '../Router'
+import { describe, it } from 'vitest'
+// import { render } from '../../tests/customRender'
+// import { screen } from '@testing-library/react'
+// import Router from '../Router'
 
 describe('Router', () => {
     it.todo('Should navigate to the home route')
