6af8eda333a5126d7f2b8dd36e707da5fb23a592
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-29 00:05:38 +0900

    add links to side drawer on mobile

--- src/components/SideDrawer/DrawerContents.tsx
+++ src/components/SideDrawer/DrawerContents.tsx
@@ -1,14 +1,39 @@
 import { FC } from 'react'
 import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
 import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import { NavLink } from 'react-router-dom'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
-interface DrawerContentsProps {
-}
+const DrawerContents: FC = () => {
+    const theme = useTheme()
+    let navList = <></>
+    if (useMediaQuery(theme.breakpoints.down('sm'))) {
+        navList = (<>
+            <ListItem>
+                <NavLink to='/'>Home</NavLink>
+            </ListItem>
+            <ListItem>
+                <NavLink to='/software'>Software</NavLink>
+            </ListItem>
+            <ListItem>
+                <NavLink to='/art'>Art</NavLink>
+            </ListItem>
+            <ListItem>
+                <NavLink to='/writing'>Writing</NavLink>
+            </ListItem>
+            <Divider />
+        </>)
+    }
 
-const DrawerContents: FC<DrawerContentsProps> = () => {
     return <Box sx={{ width: 300 }}>
         <List>
-            Todo
+            {navList}
+            <ListItem>
+                Todo
+            </ListItem>
         </List>
     </Box>
 }
