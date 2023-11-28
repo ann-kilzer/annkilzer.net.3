9edf49e1be875b64d9c17aeec7502105f34df6ff
Ann 杏 Kilzer <akilzer@gmail.com>
2023-11-28 23:56:47 +0900

    rename

--- src/components/Header/__test__/DesktopToolbar.test.tsx
+++ src/components/Header/__test__/DesktopToolbar.test.tsx
@@ -1,11 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { render } from '@/tests/customRender'
 import { screen } from '@testing-library/react'
-import Header from '../Header'
+import DesktopToolbar from '../DesktopToolbar'
 
 describe('Header', () => {
-    it('should display the Header', async () => {
-        render(<Header />)
+    it('should display the DesktopToolbar', async () => {
+        render(<DesktopToolbar />)
         const title = await screen.findByText('Ann Kilzer')
         expect(title).toBeVisible()
     })
